570cb003879174fc2c215b164cfae45492a1a77a
Harsh Maroti Gorantiwar <harshgorantiwar@gmail.com>
2026-06-15 19:31:42 +0530

    Added Day 1 J2EE homework

---
+++ Day01_15Jun2026_J2EE/Task1.java
@@ -0,0 +1,12 @@
+class Xxx{
+    public void xx(String name){
+        System.out.println("Pune");
+    }
+}
+
+class Task1{
+    public static void main(String[] args){
+        Xxx obj = new Xxx();
+        obj.xx("Maharastra");
+    }
+}
